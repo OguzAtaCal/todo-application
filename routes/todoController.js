2ac2f3134b1400c8b0713a90148952a56f7fe4a7
OguzAtaCal <ata.cal@ug.bilkent.edu.tr>
2021-06-29 02:46:31 +0300

    created todo and todo list migrations/seeds, implemented crud for todo lists

--- routes/todoController.js
+++ routes/todoController.js
@@ -1,44 +1,85 @@
-const services = require("../services/todoServices");
-
-const postOptions = {
-	schema: {
-		body: {
-			type: "object",
-			required: [],
-			properties: {
-				// todo
-			},
-		},
-	},
-};
+const todoServices = require("../services/todoListServices");
 
 module.exports = async function callBack(fastify, opts) {
+	const postOptions = {
+		schema: {
+			body: {
+				type: "object",
+				required: ["name"],
+				properties: {
+					name: { type: "string" },
+				},
+			},
+		},
+		preValidation: [fastify.jwt_authentication],
+	};
+
 	const options = {
 		preValidation: [fastify.jwt_authentication],
 	};
 
-	// get all todos
+	// get all todo lists of the specific user from the token
 	fastify.get("/todos", options, async (request, reply) => {
-		return "get todos";
+		const auth = request.headers.authorization;
+		const token = auth.split(" ")[1];
+		fastify.jwt.verify(token, (err, decoded) => {
+			if (err) {
+				console.log(err);
+				reply.send("There has been an error verifying jwt");
+			}
+			todoServices.getTodoLists(request, reply, decoded.userId);
+		});
 	});
 
-	// create a todo
-	fastify.post("/todos", options, async (request, reply) => {
-		return "post todos";
+	// create a todo list to the user with id
+	fastify.post("/todos", postOptions, async (request, reply) => {
+		const auth = request.headers.authorization;
+		const token = auth.split(" ")[1];
+		fastify.jwt.verify(token, (err, decoded) => {
+			if (err) {
+				console.log(err);
+				reply.send("There has been an error verifying jwt");
+			}
+			todoServices.createTodoList(request, reply, decoded.userId);
+		});
 	});
 
-	// get all todos of a specific user
-	fastify.get("/todos/:username", async (request, reply) => {
-		return "todos of user";
+	// get the todo list with id: if of the user id from the token
+	fastify.get("/todos/:id", options, async (request, reply) => {
+		const auth = request.headers.authorization;
+		const token = auth.split(" ")[1];
+		fastify.jwt.verify(token, (err, decoded) => {
+			if (err) {
+				console.log(err);
+				reply.send("There has been an error verifying jwt");
+			}
+			todoServices.getTodoList(request, reply, decoded.userId);
+		});
 	});
 
 	// update specific todo
 	fastify.put("/todos/:id", postOptions, async (request, reply) => {
-		return "specific todo";
+		const auth = request.headers.authorization;
+		const token = auth.split(" ")[1];
+		fastify.jwt.verify(token, (err, decoded) => {
+			if (err) {
+				console.log(err);
+				reply.send("There has been an error verifying jwt");
+			}
+			todoServices.updateTodoList(request, reply, decoded.userId);
+		});
 	});
 
 	// delete specific todo
 	fastify.delete("/todos/:id", postOptions, async (request, reply) => {
-		return "deleted todo";
+		const auth = request.headers.authorization;
+		const token = auth.split(" ")[1];
+		fastify.jwt.verify(token, (err, decoded) => {
+			if (err) {
+				console.log(err);
+				reply.send("There has been an error verifying jwt");
+			}
+			todoServices.deleteTodoList(request, reply, decoded.userId);
+		});
 	});
 };
